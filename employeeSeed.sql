0c2d7b3b2063a66d1c34296faeb22a1fc0cad65f
kpegeder <k.pegeder@gmail.com>
2020-09-30 12:15:50 -0700

    create a temporary table of employees

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -21,7 +21,7 @@ CREATE TABLE role (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE empolyee (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -29,3 +29,57 @@ CREATE TABLE empolyee (
   manager_id INTEGER(10),
   PRIMARY KEY (id)
 );
+
+INSERT INTO department (name) VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
+
+INSERT INTO role (title, salary, department_id)
+ VALUES ("Sales Lead", 100000, (SELECT id FROM department where id = 1));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, (SELECT id FROM department where id = 1));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, (SELECT id FROM department where id = 2));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, (SELECT id FROM department where id = 2));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, (SELECT id FROM department where id = 3));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 100000,(SELECT id FROM department where id = 4));
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 100000, (SELECT id FROM department where id = 4));
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", (SELECT id FROM role where id = 1));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Mike", "Chan",(SELECT id FROM role where id = 2));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Ashley", "Rodriguez",(SELECT id FROM role where id = 3));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Kevin", "Tupik",(SELECT id FROM role where id = 4));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Malia", "Brown",(SELECT id FROM role where id = 5));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Sarah", "Lourd",(SELECT id FROM role where id = 6));
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tom", "Allen",(SELECT id FROM role where id = 7));
+
+UPDATE employee
+SET manager_id = 3
+WHERE id = 1;
+
+UPDATE employee
+SET manager_id = 3
+WHERE id = 4;
+
+UPDATE employee
+SET manager_id = 6
+WHERE id = 7;
+
+UPDATE employee
+SET manager_id = 1
+WHERE id = 2;
+
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id
+FROM ((employee
+INNER JOIN role ON employee.role_id = role.id)
+INNER JOIN department ON role.department_id = department.id);
