0904f1fbffda618f30fb43ac2df6b0c26a6bcc93
kpegeder <k.pegeder@gmail.com>
2020-09-30 12:14:11 -0700

    add employee mananger gif

--- employeeCOM.js
+++ employeeCOM.js
@@ -1,5 +1,9 @@
+// Dependencies
 const mysql = require("mysql");
+const inquirer = require("inquirer");
+const addEmployee = require("./questions/addEmployee");
 
+// Create a connection to the database
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,6 +20,52 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  console.log(`
+  ,----------------------------------------------------------.
+  |      ______                 _                            |
+  |     |   __/ _ __ ___  _ __ | | ___  _   _  ___  ___      |
+  |     |   _| |  _ '  _ \\  _ \\| |/ _ \\| | | |/ _ \\/ _ \\     |
+  |     |  |___  | | | | | |_) | | (_) | |_| |  __/  __/     |
+  |     |______|_| |_| |_|  __/|_|\\___/\\___, |\\___|\\___|     |
+  |                      |__|           |___/                |
+  |                                                          |
+  |         __  __                                           |
+  |        |  \\/  | __ _ _ __   __ _  __ _  ___ _ __         |
+  |        | |\\/| |/ _' | '_ \\ / _' |/ _' |/ _ \\ '__|        |
+  |        | |  | | (_| | | | | (_| | (_| |  __/ |           |
+  |        |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           |
+  |                                  |___/                   |
+  '----------------------------------------------------------'
+    `);
+  start();
   connection.end();
 });
+
+async function start() {
+  let answers = await inquirer.prompt(introQuestion);
+  console.log(answers);
+}
+
+// Questions
+const introQuestion = [
+  {
+    name: "choice",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "Add Department",
+      "Remove Department",
+      "Update Department",
+      "Add Role",
+      "Remove Role",
+      "Exit",
+    ],
+  },
+];
