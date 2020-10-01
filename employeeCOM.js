3ba1d0b24bb346ee2f09a9135fb20e15808df465
kpegeder <k.pegeder@gmail.com>
2020-09-30 21:01:52 -0700

    create add and remove employee function

--- employeeCOM.js
+++ employeeCOM.js
@@ -38,18 +38,48 @@ connection.connect(function (err) {
   '----------------------------------------------------------'
     `);
   start();
-  connection.end();
+  // removeEmployee();
 });
 
 async function start() {
   let answers = await inquirer.prompt(introQuestion);
-  console.log(answers);
+  switch (answers.action) {
+    case "View All Employees":
+      break;
+    case "View All Employees By Department":
+      break;
+    case "View All Employees By Manager":
+      break;
+    case "Add Employee":
+      addNewEmployee();
+      break;
+    case "Remove Employee":
+      removeEmployee();
+      break;
+    case "Update Employee Role":
+      break;
+    case "Update Employee Manager":
+      break;
+    case "Add Department":
+      break;
+    case "Remove Department":
+      break;
+    case "Update Department":
+      break;
+    case "Add Role":
+      break;
+    case "Remove Role":
+      break;
+
+    default:
+      connection.end();
+  }
 }
 
 // Questions
 const introQuestion = [
   {
-    name: "choice",
+    name: "action",
     type: "list",
     message: "What would you like to do?",
     choices: [
@@ -69,3 +99,69 @@ const introQuestion = [
     ],
   },
 ];
+
+async function addNewEmployee() {
+  let newEmployee = await inquirer.prompt(addEmployee);
+  connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      first_name: newEmployee.employeeFirstName,
+      last_name: newEmployee.employeeLastName,
+      job_id: newEmployee.employeeRole,
+    },
+    function (err) {
+      if (err) throw err;
+      console.log("You succesfully added an new employee!");
+      // re-prompt the user for if they want to bid or post
+      start();
+    }
+  );
+}
+
+async function removeEmployee() {
+  connection.query("SELECT first_name, last_name FROM employee", function (
+    err,
+    results
+  ) {
+    if (err) throw err;
+    // console.log(results);
+    inquirer
+      .prompt([
+        {
+          name: "removeEmployee",
+          type: "list",
+          message: "Which employee doe you want to remove?",
+          choices: function () {
+            let choiceArr = [];
+            for (let i = 0; i < results.length; i++) {
+              choiceArr.push(
+                i + " " + results[i].first_name + " " + results[i].last_name
+              );
+              // + " " + results[i].last_name);
+            }
+            return choiceArr;
+
+            // choiceArr.push(results[i].)
+          },
+        },
+      ])
+      .then(function (response) {
+        let name = response.removeEmployee.split(" ")[0];
+        console.log(name);
+        connection.query(
+          "DELETE FROM employee WHERE ?",
+          {
+            id: name,
+          },
+          function (err, res) {
+            if (err) throw err;
+            console.log("You have removed an employee.");
+            // Call readProducts AFTER the DELETE completes
+            start();
+          }
+        );
+        // response.removeEmployee.split(" ")[1];
+        // console.log(response.removeEmployee.split(" ")[0]);
+      });
+  });
+}
