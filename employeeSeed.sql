b8d4f16a99fe655c5e7f3bf6a112bfa101755914
kpegeder <k.pegeder@gmail.com>
2020-10-01 20:07:39 -0700

    change the on cascade for fk_manager

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -30,7 +30,7 @@ CREATE TABLE employee (
   manager_id INT,
   PRIMARY KEY (id),
   CONSTRAINT FK_job FOREIGN KEY (job_id) REFERENCES job(id) ON DELETE CASCADE,
-  CONSTRAINT FK_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+  CONSTRAINT FK_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 INSERT INTO department (branch) VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
@@ -39,7 +39,7 @@ INSERT INTO job (title, salary, department_id)
 VALUES ("Sales Lead", 100000, (SELECT id FROM department where branch = "Sales")),("Salesperson", 80000, (SELECT id FROM department where branch = "Sales")), ("Lead Engineer", 150000, (SELECT id FROM department where branch = "Engineering")),("Software Engineer", 120000, (SELECT id FROM department where branch = "Engineering")),("Accountant", 125000, (SELECT id FROM department where branch = "Finance")),("Legal Team Lead", 100000,(SELECT id FROM department where branch = "Legal")),("Lawyer", 100000, (SELECT id FROM department where branch = "Finance"));
 
 INSERT INTO employee (first_name, last_name, job_id)
-VALUES ("John", "Doe", (SELECT id FROM job where title = "Sales Leads")), ("Mike", "Chan",(SELECT id FROM job where title = "Salesperson")), ("Ashley", "Rodriguez",(SELECT id FROM job where title = "Lead Engineer")), ("Kevin", "Tupik",(SELECT id FROM job where title = "Software Engineer")), ("Malia", "Brown",(SELECT id FROM job where title = "Accountant")), ("Sarah", "Lourd",(SELECT id FROM job where title = "Lega Team Lead")), ("Tom", "Allen",(SELECT id FROM job where title = "Lawyer"));
+VALUES ("John", "Doe", (SELECT id FROM job where title = "Sales Lead")), ("Mike", "Chan",(SELECT id FROM job where title = "Salesperson")), ("Ashley", "Rodriguez",(SELECT id FROM job where title = "Lead Engineer")), ("Kevin", "Tupik",(SELECT id FROM job where title = "Software Engineer")), ("Malia", "Brown",(SELECT id FROM job where title = "Accountant")), ("Sarah", "Lourd",(SELECT id FROM job where title = "Legal Team Lead")), ("Tom", "Allen",(SELECT id FROM job where title = "Lawyer"));
 
 UPDATE employee
 SET manager_id = 3
@@ -58,6 +58,6 @@ SET manager_id = 1
 WHERE id = 2;
 
 SELECT employee.id, employee.first_name, employee.last_name, job.title, job.salary, department.branch, employee.manager_id
-FROM ((employee
-INNER JOIN job ON employee.job_id = job.id)
-INNER JOIN department ON job.department_id = department.id);
+FROM employee
+INNER JOIN job ON employee.job_id = job.id
+INNER JOIN department ON job.department_id = department.id;
